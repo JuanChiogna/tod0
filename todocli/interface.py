fb348aa470309041c759a9bbb64d2b402ea918ef
Juan Chiogna <Juanchiogna@outlook.com>
2021-08-31 20:11:54 -0300

    My program
    This is my current implementation of the program. The goal is to automatically move tasks based on their due date, and posssibly create calendar events accordingly.

--- todocli/interface.py
+++ todocli/interface.py
@@ -1,372 +1,253 @@
-#!/usr/bin/env python
-from prompt_toolkit.application import Application
-from prompt_toolkit.key_binding import (
-    KeyBindings,
-    ConditionalKeyBindings,
-    merge_key_bindings,
-)
-from prompt_toolkit.filters import Condition
-from prompt_toolkit.layout.containers import (
-    HSplit,
-    VSplit,
-    Window,
-    WindowAlign,
-    DynamicContainer,
-)
-from prompt_toolkit.layout.controls import FormattedTextControl
-from prompt_toolkit.layout.layout import Layout
-from prompt_toolkit.widgets import TextArea
+import auth
+import time
+from datetime import datetime
 
-from todocli import auth
+class Tasklist():
+    def __init__(self, data):
+        self.data = data
+        self.displayName = data.get('displayName', '')
+        self.id = data.get('id', '')
+        self.get_tasks()
 
-# Colors
-color_folder = "bg:#006699"
-color_task = "bg:#006699"
+    def delete(self):
+        # Deletes the tasklist from To Do
+        if auth.delete_tasklist(self.id):
+            print(f">> Deleted tasklist '{self.displayName}'.")
+            return True
+        else:
+            print(f">> Could not delete tasklist '{self.displayName}'.")
+            return False
 
-focus_index_folder = 0
-focus_index_task = 0
-focus_folder = True
+    def change_title(self, title):
+        # Updates the current title
+        old_title = self.displayName
+        self.displayName = title
+        request_body = {"displayName": title}
+        if auth.update_tasklist(self.id, request_body):
+            print(f">> Updated tasklist title from '{old_title}' to '{title}'.")
+        else:
+            print(f">> Could not update tasklist title from '{old_title}' to '{title}'.")
 
-# Confirmation stuff
-waiting_for_confirmation = False
+    def get_tasks(self):
+        # Loads all tasks from the tasklist
+        # print("\n" + self.displayName + "\n")
+        self.tasks = []
+        task_data = auth.list_tasks(self.id)
+        for tsk in task_data:
+            task = Task(self.displayName, self.id, tsk)
+            if task.status != "completed":
+                # print("    " + task.title)
+                # print(task.dueDateTime)
+                # if task.dueDateTime != "":
+                #     print()
+                self.tasks.append(task)
 
+    def tasks_remaining(self):
+        done = True
+        for task in self.tasks:
+            if task.dueDate == date_today and self.displayName != "Tareas":
+                done = False
+        return not done
 
-@Condition
-def is_not_waiting_for_confirmation():
-    "Enable key bindings when not waiting for confirmation"
-    return not waiting_for_confirmation
-
-
-# Global data structures to hold info
-folder2id = {}
-folders = []
-task2id = {}
-tasks = []
-
-
-def load_folders():
-    """
-    Load all folders
-    """
-    global left_window
-    global focus_index_folder
-    global folder2id
-    global folders
-
-    # Reset all folder data structures
-    focus_index_folder = 0
-    folder2id.clear()
-    folders.clear()
-
-    # Retrieve folder data
-    folder_data = auth.list_and_update_folders()
-    for idx, f in enumerate(folder_data):
-        folder2id[idx] = f["id"]
-        folders.append(f["name"])
-
-    # Layout interface
-    left_window.children = [
-        Window(FormattedTextControl(f), height=1, width=50) for f in folders
-    ]
-
-    # Highlight first folder
-    left_window.children[0].style = color_folder
+    def delete_task(self, task_id, move_task=False):
+        # Deletes a given task from the tasklist
+        task = task_by_id(self.id, task_id)
+        if task is None:
+            return
+        idx = self.tasks.index(task)
+        if task.delete():
+            if not move_task:
+                print(f">> Deleted task '{task.title}' from  tasklist '{self.displayName}'.")
+            self.tasks.pop(idx)
+            return True
+        else:
+            if not move_task:
+                print(f">> Could not delete task '{task.title}' from the list '{self.displayName}'.")
+            return False
 
 
-left_window = HSplit([Window()])
+class Task():
+    def __init__(self, tasklist_displayName, tasklist_id, data):
+        self.data = data
+        self.title = data.get('title', '')
+        self.tasklist_displayName = tasklist_displayName
+        self.status = data.get('status', '')
+        self.createdDateTime = data.get('createdDateTime', '')
+        self.lastModifiedDateTime = data.get('lastModifiedDateTime', '')
+        self.id = data.get('id', '')
+        self.tasklist_id = tasklist_id
+        self.body = data.get('body', '')
+        self.dueDateTime = data.get('dueDateTime', '')
+        self.dueDate = get_date(self.data)
 
-# Get folders to load on screen
-load_folders()
+    def toggle(self):
+        # Toggles task between checked and unchecked
+        if self.status == "completed":
+            # If it's checked, uncheck it
+            if auth.update_task(self.tasklist_id, self.id, {"status": "notStarted"}):
+                self.status = "notStarted"
+                print(f">> Changed status of '{self.title}' from 'completed' to 'notStarted'.")
+            else:
+                print(f">> Could not change status of '{self.title}' from 'completed' to 'notStarted'.")
+        elif self.status == "notStarted":
+            # If it's unchecked, check it
+            if auth.update_task(self.tasklist_id, self.id, {"status": "completed"}):
+                self.status = "completed"
+                print(f">> Changed status of '{self.title}' from 'notStarted' to 'completed'.")
+            else:
+                print(f">> Could not change status of '{self.title}' from 'notStarted' to 'completed'.")
+        else:
+            input(f">> Can't do or undo task: {self.title}.")
 
-right_window = HSplit([Window()])
-body = VSplit(
-    [
-        left_window,
-        Window(width=1, char="|", style="class:line"),
-        right_window,
-    ]
-)
+    def delete(self):
+        # Deletes the task from To Do
+        if auth.delete_task(self.tasklist_id, self.id):
+            return True
+        else:
+            return False
 
-
-def get_titlebar_text():
-    return [("class:title", " tod0 ")]
-
-
-prompt_window = Window(
-    height=1, content=FormattedTextControl(""), align=WindowAlign.LEFT
-)
-
-root_container = HSplit(
-    [
-        # The titlebar.
-        Window(
-            height=1,
-            content=FormattedTextControl(get_titlebar_text),
-            align=WindowAlign.CENTER,
-        ),
-        # Horizontal separator.
-        Window(height=1, char="-", style="class:line"),
-        # The 'body', like defined above.
-        body,
-        Window(height=1, char=".", style="class:line"),
-        DynamicContainer(lambda: prompt_window),
-    ]
-)
-
-
-# Key bindings
-kb = KeyBindings()
-kb_exit = KeyBindings()
-kb_escape = KeyBindings()
-
-
-@kb_exit.add("c-c", eager=True)
-@kb_exit.add("c-q", eager=True)
-def _(event):
-    """
-    Pressing Ctrl-Q or Ctrl-C will exit the user interface.
-    """
-    event.app.exit()
+    def set_due_date(self, dueDate):
+        # Sets a due date for a task given in format "dd-mm-yyyy"
+        reversed_dueDate = reverse_date(dueDate)
+        request_body = {"dueDateTime": {"dateTime": f"{reversed_dueDate}T03:00:00.0000000", "timeZone": "UTC"}}
+        if auth.update_task(self.tasklist_id, self.id, request_body):
+            print(f">> Setted due date of task '{self.title}' to '{dueDate}'.")
+            return True
+        else:
+            print(f">> Could not set due date of task '{self.title}' to '{dueDate}'.")
+            return False
 
 
-@kb.add("j")
-def _(event):
-    """
-    Move selection down 1
-    """
+class Calendar():
+    pass
 
-    if focus_folder:
-        global focus_index_folder
-        folder_window = left_window.children
-        folder_window[focus_index_folder].style = ""
-        focus_index_folder = (focus_index_folder + 1) % len(folders)
-        folder_window[focus_index_folder].style = color_folder
+
+class Event():
+    pass
+
+def tasklist_by_name(name):
+    # Returns tasklist instance if found by name
+    global tasklists
+    for tasklist in tasklists:
+        if tasklist.displayName == name:
+            return tasklist
     else:
-        global focus_index_task
-        if tasks:
-            tasks[focus_index_task].style = ""
-            focus_index_task = (focus_index_task + 1) % len(tasks)
-            tasks[focus_index_task].style = color_task
+        print(f">> No tasklist with name '{name}' exists.")
+        return None
 
-
-@kb.add("k")
-def _(event):
-    """
-    Move selection up 1
-    """
-
-    if focus_folder:
-        global focus_index_folder
-        folder_window = left_window.children
-        folder_window[focus_index_folder].style = ""
-        focus_index_folder = (focus_index_folder - 1) % len(folders)
-        folder_window[focus_index_folder].style = color_folder
+def tasklist_by_id(tasklist_id):
+    # Returns tasklist instance if found by id
+    global tasklists
+    for tasklist in tasklists:
+        if tasklist.id == tasklist_id:
+            return tasklist
     else:
-        global focus_index_task
-        if tasks:
-            tasks[focus_index_task].style = ""
-            focus_index_task = (focus_index_task - 1) % len(tasks)
-            tasks[focus_index_task].style = color_task
+        print(f">> No tasklist with id '{tasklist_id}' exists.")
+        return None
 
-
-@kb.add("l")
-def _(event):
-    """
-    Select currently focused folder
-    """
-    if focus_folder:
-        load_tasks()
-
-
-@kb.add("h")
-def _(event):
-    """
-    Go back to folder scroll mode
-    """
-    global focus_index_task
-    global focus_folder
-    global right_window
-
-    if tasks:
-        tasks[focus_index_task].style = ""
-    right_window.children = [Window()]
-    focus_folder = True
-
-
-@kb.add("c")
-def _(event):
-    """
-    Mark task as complete
-    """
-
-    # Only receive input on task view mode
-    if focus_folder or (not focus_folder and not tasks):
-        return
-
-    global waiting_for_confirmation
-    global prompt_window
-
-    waiting_for_confirmation = True
-
-    input_field = TextArea(
-        height=1,
-        prompt="Mark task as complete? <y> to confirm. ",
-        style="class:input-field",
-        multiline=False,
-        wrap_lines=False,
-    )
-
-    # Confirmation of commands
-    def confirm(buff):
-        global waiting_for_confirmation
-        global prompt_window
-        user_input = input_field.text
-        if user_input == "y":
-            # Mark task as complete
-            auth.complete_task(task2id[focus_index_task])
-            load_tasks()
-
-        # Return to normal state
-        waiting_for_confirmation = False
-        prompt_window = Window()
-
-    input_field.accept_handler = confirm
-
-    prompt_window = input_field
-    event.app.layout.focus(input_field)
-
-
-@kb.add("n")
-def _(event):
-    """
-    Create new task/folder
-    """
-    global waiting_for_confirmation
-    global prompt_window
-
-    waiting_for_confirmation = True
-
-    # Check if we are creating new task or folder
-    if focus_folder:
-        # We are creating a new folder
-        input_field = TextArea(
-            height=1,
-            prompt="New folder: ",
-            style="class:input-field",
-            multiline=False,
-            wrap_lines=False,
-        )
-
-        # Get new folder name
-        def get_name(buff):
-            global waiting_for_confirmation
-            global prompt_window
-            global left_window
-
-            user_input = input_field.text
-            if user_input:
-                # Create new folder
-                auth.create_folder(user_input)
-                # Refresh folders
-                load_folders()
-
-            # Return to normal state
-            waiting_for_confirmation = False
-            prompt_window = Window()
-
+def task_by_name(tasklist_id, name):
+    # Returns task instance if found by name
+    tasklist = tasklist_by_id(tasklist_id)
+    for task in tasklist.tasks:
+        if task.title == name:
+            return task
     else:
-        # We are creating a new task
-        input_field = TextArea(
-            height=1,
-            prompt="New task: ",
-            style="class:input-field",
-            multiline=False,
-            wrap_lines=False,
-        )
+        print(f">> No task with name '{name}' exists in '{tasklist.displayName}'.")
+        return None
 
-        # Get new task name
-        def get_name(buff):
-            global waiting_for_confirmation
-            global prompt_window
-            user_input = input_field.text
-            if user_input:
-                # Create new task
-                auth.create_task(user_input, folder2id[focus_index_folder])
-                # Refresh tasks
-                load_tasks()
-
-            # Return to normal state
-            waiting_for_confirmation = False
-            prompt_window = Window()
-
-    input_field.accept_handler = get_name
-
-    prompt_window = input_field
-    event.app.layout.focus(input_field)
-
-
-@kb_escape.add("escape", eager=True)
-def _(event):
-    """
-    Escape prompt
-    """
-    global waiting_for_confirmation
-    global prompt_window
-
-    # Return to normal state
-    waiting_for_confirmation = False
-    prompt_window = Window()
-
-
-kb = ConditionalKeyBindings(kb, is_not_waiting_for_confirmation)
-
-
-def load_tasks():
-    """
-    Load tasks of currently focused folder
-    """
-
-    global focus_folder
-    global focus_index_task
-    global tasks
-    global task2id
-
-    task_data = auth.list_tasks(all_=False, folder=folder2id[focus_index_folder])
-
-    tasks = []
-    for idx, t in enumerate(task_data):
-        id_ = t["id"]
-        subject = t["subject"]
-        status = t["status"]
-        folder_id = t["parentFolderId"]
-        tasks.append(Window(FormattedTextControl(subject), height=1))
-        task2id[idx] = id_
-
-    # Add empty container if task list is empty
-    if not tasks:
-        right_window.children = [Window(FormattedTextControl("-- No Tasks --"))]
+def task_by_id(tasklist_id, task_id):
+    # Returns task instance if found by id
+    tasklist = tasklist_by_id(tasklist_id)
+    for task in tasklist.tasks:
+        if task.id == task_id:
+            return task
     else:
-        right_window.children = tasks
-        focus_index_task = 0
-        tasks[focus_index_task].style = color_task
-    focus_folder = False
+        print(f">> No task with id '{task_id}' exists in '{tasklist.displayName}'.")
+        return None
 
 
-# Creating an `Application` instance
-# ----------------------------------
-application = Application(
-    layout=Layout(root_container),
-    key_bindings=merge_key_bindings([kb, kb_exit, kb_escape]),
-    mouse_support=False,
-    full_screen=False,
-)
+def load_lists():
+    # Loads all tasklists and their respective tasks
+    tasklists = []
+    tasklist_data = auth.list_tasklists()
+    for lst in tasklist_data:
+        tasklist = Tasklist(lst)
+        tasklists.append(tasklist)
+    return tasklists
 
 
-# Run the application
-# -------------------
+def move_task(tasklist_id_1, tasklist_id_2, task_id):
+    # Moves a task from tasklist 1 to tasklist 2
+    global tasklists
+    # Avoid moving to the same tasklist
+    if tasklist_id_1 == tasklist_id_2:
+        return False
+    tasklist_1 = tasklist_by_id(tasklist_id_1)
+    tasklist_2 = tasklist_by_id(tasklist_id_2)
+    # Check that both tasklists exist
+    if tasklist_1 is None or tasklist_2 is None:
+        return False
+    task = task_by_id(tasklist_id_1, task_id)
+    # Check that the task exists
+    if task is None:
+        return False
+    # Create a copy of the task at the new list
+    if auth.create_task(tasklist_id_2, task.title, task.data):
+        # Delete the task at the previous list
+        tasklist_1.delete_task(task_id, move_task=True)
+        print(f">> Moved task '{task.title}' from '{tasklist_1.displayName}' to '{tasklist_2.displayName}'.")
+        return True
+    else:
+        print(f">> Could not move task '{task.title}' from '{tasklist_1.displayName}' to '{tasklist_2.displayName}'.")
+        return False
+    
+
+def get_date(request_body):
+    # Takes a task's request_body and returns its due date in format "dd-mm-yyyy"
+    date_dict = request_body.get("dueDateTime", {})
+    if date_dict != {}:
+        date = date_dict.get("dateTime", "")
+        date = date.replace("T00:00:00.0000000", "")
+        date = date.replace("T03:00:00.0000000", "")
+    else:
+        date = ""
+    return reverse_date(date)
+
+def reverse_date(date):
+    # Takes a date with format "yyyy-mm-dd" and returns "dd-mm-yyyy" or vice versa
+    reversed_date = date.split("-")
+    reversed_date.reverse()
+    reversed_date = "-".join(reversed_date)
+    return reversed_date
+
 def run():
-    application.run()
+    # 1.1 Load tasks
+    global tasklists
+    print(">> Loading tasks . . .")
+    tasklists = load_lists()
+    print(">> Tasks loaded successfully")
 
+    # 1.2 Load events
 
-if __name__ == "__main__":
-    run()
+    # 2. Get DateTime format of today
+    global date_today
+    dt = datetime.today()
+    day, month, year = [f"0{x}" if len(str(x)) < 2 else f"{x}" for x in (dt.day, dt.month, dt.year)]
+    date_today = "-".join([day, month, year])
+    print(f">> Today's date is {date_today}")
+
+    # 3.1 Move tasks accordingly
+    tareas = tasklist_by_name("Tareas")
+    for tasklist in tasklists:
+        loop = 0
+        while tasklist.tasks_remaining():
+            for task in tasklist.tasks:
+                if task.dueDate == date_today:
+                    move_task(tasklist.id, tareas.id, task.id)
+            loop += 1
+            if loop >= 50:
+                print(f">> Stuck on infinite loop on list {tasklist.displayName}")
+                break
+    print(">> Done!")
+
+    # 3.2 Create events accordingly
