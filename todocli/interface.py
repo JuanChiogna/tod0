0331d2d78588ed25454370f529462d1343fd4234
Juan Chiogna <Juanchiogna@outlook.com>
2021-09-05 15:22:48 -0300

    Fixed focus loss when starting

--- todocli/interface.py
+++ todocli/interface.py
@@ -2,40 +2,42 @@ import tkinter as tk
 import os
 
 def get_value(event):
-    global input_value, entry_box, root
+    global input_value, entry_box, window
     input_value = entry_box.get()
-    root.destroy()
+    window.destroy()
 
 def center_window(width=300, height=200):
     # get screen width and height
-    screen_width = root.winfo_screenwidth()
-    screen_height = root.winfo_screenheight()
+    screen_width = window.winfo_screenwidth()
+    screen_height = window.winfo_screenheight()
 
     # calculate position x and y coordinates
     x = (screen_width/2) - (width/2)
     y = (screen_height/4) - (height/2)
-    root.geometry('%dx%d+%d+%d' % (width, height, x, y))
+    window.geometry('%dx%d+%d+%d' % (width, height, x, y))
 
 def run():
-    global input_value, entry_box, root
+    global input_value, entry_box, window
 
-    root = tk.Tk()
-    root.title("Microsoft To Do: Quick Task")
-    root.iconbitmap(f"{os.path.dirname(os.path.abspath(__file__))}\\Microsoft-To-Do.ico")
-    root.focus_force()
+    window = tk.Tk()
+    window.title("Microsoft To Do: Quick Task")
+    window.iconbitmap(f"{os.path.dirname(os.path.abspath(__file__))}\\Microsoft-To-Do.ico")
+    window.attributes("-topmost", True)
+    window.lift()
 
     input_value = None
 
-    entry_box = tk.Entry(root, font = "SegoeUI 25", fg = "white", width = 50, borderwidth=5)
+    entry_box = tk.Entry(window, font = "SegoeUI 25", fg = "white", width = 50, borderwidth=5)
     entry_box.configure({"background": "#292929"})
     entry_box.pack()
     entry_box.focus()
 
-    root.bind("<Return>", get_value)
-    root.bind("<Escape>", lambda x: root.destroy())
+    window.bind("<Return>", get_value)
+    window.bind("<Escape>", lambda x: window.destroy())
 
+    window.after(1000, entry_box.focus)
     center_window(600, 50)
-    root.mainloop()
+    window.mainloop()
     return input_value
 
 if __name__ == "__main__":
